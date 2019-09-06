30d49fa269e472257107c4b4ca0888914553c5a4
c2997108 <c2997108@gmail.com>
2019-09-06 18:27:36 +0900

    0.9

--- PortablePipeline/src/application/ScriptCell.java
+++ PortablePipeline/src/application/ScriptCell.java
@@ -1,9 +1,13 @@
 package application;
 
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.Tooltip;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 
 public class ScriptCell extends ListCell<ScriptNode> {
@@ -29,6 +33,38 @@ public class ScriptCell extends ListCell<ScriptNode> {
 
 
         	HBox hBox = new HBox(20d);
+
+
+        	String baseUrl = "http://suikou.fs.a.u-tokyo.ac.jp/pp/";
+        	Button button1 = new Button("", new ImageView(new Image("file:image/iconmonstr-share-8-24.png", 12, 12, false, false)));
+        	try {
+        		button1.setOnAction((ActionEvent e) -> {
+        			System.out.println(jNode.filename);
+        			final String OS_NAME = System.getProperty("os.name").toLowerCase();
+        			try {
+        				Runtime rt = Runtime.getRuntime();
+        				String cmd = "";
+        				if(OS_NAME.startsWith("windows")) {
+        					cmd = "cmd /c start "+baseUrl+"/"+jNode.filename+"/index.html";
+        				}else if(OS_NAME.startsWith("mac")) {
+        					cmd = "open "+baseUrl+"/"+jNode.filename+"/index.html";
+        				}else if(OS_NAME.startsWith("linux")) {
+        					cmd = "firefox "+baseUrl+"/"+jNode.filename+"/index.html";
+        				}
+        				System.out.println(cmd);
+        				rt.exec(cmd);
+        			} catch (Exception e2) {
+        				e2.printStackTrace();
+        			}
+        		});
+        	}catch(Exception e) {
+        		e.printStackTrace();
+        	}
+        	Tooltip tooltipbutton1 = new Tooltip("Open an exapmle");
+        	Tooltip.install(button1, tooltipbutton1);
+
+            hBox.getChildren().add(button1);
+
         	label1.setMinWidth(300d);
         	label2.setMaxHeight(15d);
         	Tooltip tooltip = new Tooltip(jNode.explanation);
