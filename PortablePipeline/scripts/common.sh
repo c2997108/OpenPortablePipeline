d87611edbd6e4a1646d9d20ee82c599056184ef5
c2997108 <c2997108@gmail.com>
2022-03-31 14:12:27 +0900

    1.0.4

--- PortablePipeline/scripts/common.sh
+++ PortablePipeline/scripts/common.sh
@@ -54,11 +54,11 @@ DIR_WORK="."
 
 
 #CON_DOCKER='docker run -v $PWD:$PWD -w $PWD -u root -i --rm '
-CON_DOCKER='PPDOCNAME=pp`date +%Y%m%d_%H%M%S_%3N`_$RANDOM; echo $PPDOCNAME >> '"$workdir"'/pp-docker-list; docker run --name ${PPDOCNAME} -v $PWD:$PWD -w $PWD -u '`id -u`':'`id -g`' -i --rm '
+CON_DOCKER='PPDOCNAME=pp`date +%Y%m%d_%H%M%S_%3N`_$RANDOM; echo $PPDOCNAME >> '"$workdir"'/pp-docker-list; docker run --name ${PPDOCNAME} -v $PWD:$PWD -w $PWD '"$PPDOCBINDS"' -u '`id -u`':'`id -g`' -i --rm '
 if [ "`echo $PWD|grep '^/home'|wc -l`" = 1 ]; then
- CON_SING="singularity exec "
+ CON_SING="singularity exec $PPSINGBINDS "
 else
- CON_SING="singularity exec -B $PWD:/mnt --pwd /mnt "
+ CON_SING="singularity exec -B $PWD:/mnt --pwd /mnt $PPSINGBINDS "
 fi
 CHECK_SING="singularity"
 
@@ -232,6 +232,35 @@ for i in `echo "$optiondef"|tail -n+2|awk '{if(NR>1){print old}; old=$0}'|awk -F
  export $i="$(eval echo \${$i:-$k})";
 # export $i="`eval echo \\\$$i`";
 done
+
+#input file realpath check
+echo "Checking the realpath of input files."
+PPINDIRS=()
+for i in `set|grep "^input_[0-9]\+="`; do
+ PPINDIRTMP=`echo "$i"|sed 's/^input_[0-9]\+=//'`;
+ if [ "$PPINDIRTMP" != "" ]; then
+  for j in `find "$PPINDIRTMP"`; do
+   echo "$j"
+   PPINDIRTMP2=`readlink "$j" || true`;
+   #if [ "$PPINDIRTMP2" != "" ]; then echo "$PPINDIRTMP2"; fi;
+   while [ "$PPINDIRTMP2" != "" ]; do
+    if [ -d "$PPINDIRTMP2" ]; then PPINDIRS+=(`realpath "$PPINDIRTMP2"`); else PPINDIRTMP3=`dirname "$PPINDIRTMP2"`; PPINDIRS+=(`realpath "$PPINDIRTMP3"`); fi;
+    PPINDIRTMP2=`readlink "$PPINDIRTMP2" || true`;
+    #if [ "$PPINDIRTMP2" != "" ]; then echo "$PPINDIRTMP2"; fi;
+   done;
+  done
+ fi
+done
+IFS=$'\n' PPINDIRS=(`printf "%s\n" "${PPINDIRS[@]}" |sort -u`);
+unset IFS
+PPDOCBINDS=""
+PPSINGBINDS=""
+for i in "${PPINDIRS[@]}"; do
+ echo "$i";
+ PPDOCBINDS="$PPDOCBINDS -v $i:$i"
+ PPSINGBINDS="$PPSINGBINDS -B $i:$i"
+done
+
 
 
 N_CPU=`cat /proc/cpuinfo 2> /dev/null |grep ^processor|wc -l` #all CPU
