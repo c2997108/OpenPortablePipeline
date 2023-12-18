93924e8d19d759e14f84eaa3f6c6babf43bee9c5
c2997108 <c2997108@gmail.com>
2023-12-18 18:49:04 +0900

    1.3.0

--- PortablePipeline/PortablePipeline.bat
+++ PortablePipeline/PortablePipeline.bat
@@ -1,4 +1,5 @@
 :$curdir=Get-Location
 :$cdcmd='cd '+$curdir+';'
 
-powershell start-process powershell -verb runas -ArgumentList '-Command', 'cd ', %~dp0, ';', '.\java\8\bin\java -jar PortablePipeline.jar'
+powershell start-process powershell -verb runas -ArgumentList '-Command', 'cd ', %~dp0, ';', '.\jdk-21.0.1\bin\java --module-path "./javafx-sdk-21.0.1/lib/" --add-modules javafx.controls,javafx.fxml -jar PortablePipeline.jar'
+
--- PortablePipeline/PortablePipeline.command
+++ PortablePipeline/PortablePipeline.command
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd `dirname $0`
-./jdk-21.0.1.aarch64.jdk/bin/java --module-path "./javafx-sdk-21.0.1.aarch64/lib" --add-modules javafx.controls,javafx.fxml -jar PortablePipeline.jar
+./jdk-21.0.1/bin/java --module-path "./javafx-sdk-21.0.1/lib/" --add-modules javafx.controls,javafx.fxml -jar PortablePipeline.jar
